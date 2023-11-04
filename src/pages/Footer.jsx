7bf2b4f1ae1a7a12c850065388dfab98056d18a2
ehmasuk <ehmasuk@gmail.com>
2023-11-05 02:32:04 +0600

    Updates

--- src/pages/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <div className="w-100 p-4 bg-dark text-light text-center">
-            <b>@copyright 2023 footer</b>
-        </div>
-    );
-}
-
-export default Footer;
